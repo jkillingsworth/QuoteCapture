331cdcf255d53fa4b7307037c24814a5179480e6
Jim Killingsworth <jkillingsworth@gmail.com>
2016-07-07 19:38:58 -0700

    Add logging module skeleton

---
+++ src/QuoteCapture/Logging.fs
@@ -0,0 +1,28 @@
+﻿module QuoteCapture.Logging
+
+open System
+
+//-------------------------------------------------------------------------------------------------
+
+type Log() =
+
+    static member Extra(message : string) =
+        ()
+
+    static member Trace(message : string, [<ParamArray>] args : obj[]) =
+        ()
+
+    static member Debug(message : string, [<ParamArray>] args : obj[]) =
+        ()
+
+    static member Info(message : string, [<ParamArray>] args : obj[]) =
+        ()
+
+    static member Warn(message : string, [<ParamArray>] args : obj[]) =
+        ()
+
+    static member Error(ex : Exception) =
+        ()
+
+    static member Fatal(ex : Exception) =
+        ()
