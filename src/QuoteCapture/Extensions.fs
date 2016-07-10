9fba080af077dd7b46ce2b11df5f3eb6f2bbbd16
Jim Killingsworth <jkillingsworth@gmail.com>
2016-07-10 00:01:12 -0700

    Add extensions module

---
+++ src/QuoteCapture/Extensions.fs
@@ -0,0 +1,11 @@
+﻿module QuoteCapture.Extensions
+
+open System.Text.RegularExpressions
+
+//-------------------------------------------------------------------------------------------------
+
+type Match with
+
+    member this.GetValue(index : int, group : string) =
+
+        this.Groups.[group].Captures.[index].Value
