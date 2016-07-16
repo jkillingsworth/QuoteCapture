d29a2be0c8019d9f6dc0294a3ea308d3d136c4b1
Jim Killingsworth <jkillingsworth@gmail.com>
2016-07-16 16:02:23 -0700

    Update main function to accept command line arguments

--- src/QuoteCapture.Yahoo/Program.fs
+++ src/QuoteCapture.Yahoo/Program.fs
@@ -1,10 +1,33 @@
 ﻿module Program
 
+open System
 open QuoteCapture.Yahoo
 
 //-------------------------------------------------------------------------------------------------
 
 [<EntryPoint>]
-let main argv =
-    use client = new QuoteClient.Client()
-    0
+let main = function
+
+    | [| "update"; date; ticker |]
+        ->
+        let date = DateTime.ParseExact(date, "d", null)
+        let issue = Persistence.selectIssueByTicker ticker
+        Update.updateOne date issue
+        0
+
+    | [| "update"; date |]
+        ->
+        let date = DateTime.ParseExact(date, "d", null)
+        Update.updateAll date
+        0
+
+    | [| "update" |]
+        ->
+        let date = Date.getMaximumDate ()
+        Update.updateAll date
+        0
+
+    | argv
+        ->
+        failwith "Invalid parameters."
+        1
