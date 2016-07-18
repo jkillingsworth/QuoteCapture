506c6a779c26ee15abe2f441f2cb74018a56ce4b
Jim Killingsworth <jkillingsworth@gmail.com>
2016-07-18 14:35:42 -0700

    Add date module

---
+++ src/QuoteCapture.Oanda/Date.fs
@@ -0,0 +1,20 @@
+﻿module QuoteCapture.Oanda.Date
+
+open System
+
+//-------------------------------------------------------------------------------------------------
+
+let private minimumDate = DateTime(1970, 01, 01)
+let private addDays days (date : DateTime) = date.AddDays(float days)
+
+//-------------------------------------------------------------------------------------------------
+
+let getMinimumDate () = minimumDate
+
+let getMaximumDate () = DateTime.UtcNow.Date |> addDays -1
+
+let getNextDate = function
+    | Some date
+        -> date |> addDays +1
+    | None
+        -> getMinimumDate ()
