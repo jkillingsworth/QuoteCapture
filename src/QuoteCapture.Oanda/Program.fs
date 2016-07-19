fac98f6e72560699cc299892c3ec9dd11e75d434
Jim Killingsworth <jkillingsworth@gmail.com>
2016-07-18 17:22:01 -0700

    Update main function to accept command line arguments

--- src/QuoteCapture.Oanda/Program.fs
+++ src/QuoteCapture.Oanda/Program.fs
@@ -1,10 +1,39 @@
 ﻿module Program
 
+open System
 open QuoteCapture.Oanda
 
 //-------------------------------------------------------------------------------------------------
 
 [<EntryPoint>]
-let main argv =
-    use client = new QuoteClient.Client()
-    0
+let main = function
+
+    | [| "update"; date; pair |]
+        ->
+        let date = DateTime.ParseExact(date, "d", null)
+        let currencyNames = pair.Split('/')
+        let currencyNameBase = currencyNames.[0]
+        let currencyNameQuot = currencyNames.[1]
+        let pair = Persistence.selectPairByCurrencyNames currencyNameBase currencyNameQuot
+        Update.updateOne date pair
+        Update.updateInterestRates ()
+        0
+
+    | [| "update"; date |]
+        ->
+        let date = DateTime.ParseExact(date, "d", null)
+        Update.updateAll date
+        Update.updateInterestRates ()
+        0
+
+    | [| "update" |]
+        ->
+        let date = Date.getMaximumDate ()
+        Update.updateAll date
+        Update.updateInterestRates ()
+        0
+
+    | argv
+        ->
+        failwith "Invalid parameters."
+        1
